096f1c75b64a0fcd79fe535e6a04671fc34ba646
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-02 03:36:44 -0600

    Create a floating button and an image gallery component

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,5 +1,8 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
+import { NoteView, NothingSelectedView } from '../views';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NoteView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -10,8 +13,24 @@ export const JournalPage = () => {
         natus voluptates error dolores obcaecati! Expedita.
       </Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+        color="primary"
+      >
+        {' '}
+        <AddOutlined />
+      </IconButton>
     </JournalLayout>
   );
 };
