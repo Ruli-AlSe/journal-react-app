fb36999150399899f650aaaf5b1bee2ddd51808e
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-02 02:56:06 -0600

    Create component when nothing has been selected

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,15 +1,16 @@
-import { Typography } from '@mui/material';
-
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta officia praesentium tenetur
         similique totam dicta quis exercitationem! Id eum sapiente quae ipsum quibusdam corporis
         natus voluptates error dolores obcaecati! Expedita.
-      </Typography>
+      </Typography> */}
+
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
