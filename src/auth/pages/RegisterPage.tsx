f6a9c9f2671bf984c27dd5b6c9c1dbb3efc2da03
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-06 03:36:11 -0600

    Use the useForm hook to implement functionality to create basic validations on form fields

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,25 +1,85 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid2, Link, TextField, Typography } from '@mui/material';
+
 import { AuthLayout } from '../layout/AuthLayout';
+import { FormValidationRules, useForm } from '../../hooks';
+import { useState } from 'react';
+
+const formData = {
+  email: '',
+  password: '',
+  displayName: '',
+};
+
+const formValidations: FormValidationRules<typeof formData> = {
+  email: [(value: string) => value.includes('@'), 'The email must have an @'],
+  password: [(value: string) => value.length >= 6, 'The password must have more than 6 letters'],
+  displayName: [(value: string) => value.length >= 1, 'The name is required'],
+};
 
 export const RegisterPage = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { formState, onInputChange, isFormValid, displayNameValid, emailValid, passwordValid } =
+    useForm(formData, formValidations);
+  const { email, password, displayName } = formState;
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+
+    if (!isFormValid) return;
+
+    console.log(formState);
+  };
+
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid2 container direction="column">
           <Grid2 container sx={{ mt: 2 }}>
-            <TextField label="Full Name" type="text" placeholder="John Doe" fullWidth />
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="John Doe"
+              fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={!!displayNameValid && formSubmitted}
+              helperText={displayNameValid}
+            />
           </Grid2>
           <Grid2 container sx={{ mt: 2 }}>
-            <TextField label="Email" type="email" placeholder="email@example.com" fullWidth />
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@example.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={!!emailValid && formSubmitted}
+              helperText={emailValid}
+            />
           </Grid2>
           <Grid2 container sx={{ mt: 2 }}>
-            <TextField label="Password" type="password" placeholder="password" fullWidth />
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="password"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={!!passwordValid && formSubmitted}
+              helperText={passwordValid}
+            />
           </Grid2>
 
           <Grid2 container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid2 size={{ xs: 12 }}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth type="submit">
                 Create Account
               </Button>
             </Grid2>
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = <T>(initialState: T) => {
+export type FormValidationRules<T extends {}> = {
+  [key in keyof T]: [(value: any) => boolean, string];
+};
+
+type FormValidations<T extends {}> = {
+  [key in keyof T as `${string & key}Valid`]: string | null;
+};
+
+export const useForm = <T extends {}>(
+  initialState: T,
+  formValidationRules?: FormValidationRules<T>
+) => {
   const [formState, setFormState] = useState(initialState);
+  const [formValidation, setFormValidation] = useState<FormValidations<T>>(
+    {} as FormValidations<T>
+  );
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+
+  const isFormValid = useMemo(() => {
+    for (const formValue of Object.keys(formValidation)) {
+      if (formValidation[formValue as keyof FormValidations<T>] !== null) return false;
+    }
+
+    return true;
+  }, [formValidation]);
 
   const onInputChange = ({ target }: any) => {
     const { name, value } = target;
@@ -15,9 +41,26 @@ export const useForm = <T>(initialState: T) => {
     setFormState(initialState);
   };
 
+  const createValidators = () => {
+    if (!formValidationRules) return;
+    const formCheckValues = {} as FormValidations<T>;
+
+    for (const formField of Object.keys(formValidationRules) as (keyof T)[]) {
+      const [fn, errorMessage] = formValidationRules[formField];
+
+      formCheckValues[`${String(formField)}Valid` as keyof FormValidations<T>] = (
+        fn(formState[formField]) ? null : errorMessage
+      ) as FormValidations<T>[keyof FormValidations<T>];
+    }
+
+    setFormValidation(formCheckValues as FormValidations<T>);
+  };
+
   return {
     formState,
     onInputChange,
     onResetForm,
+    isFormValid,
+    ...formValidation,
   };
 };
